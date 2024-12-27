0e1eb7eae14a486b70cc105f38856474e115b1d8
Liniker Patricio Nascimento da Costa <124075973+LinikerB@users.noreply.github.com>
2024-12-27 12:06:34 -0400

    Add files via upload

---
+++ src/ApiParaConversao.java
@@ -0,0 +1,33 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiParaConversao {
+
+    public static void Moeda(Referencias referencias){
+        String chaveApi = "ee11cac90cb78071e36d86b3";
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+        try{
+            URI endereco = URI.create("https://v6.exchangerate-api.com/v6/"+chaveApi+"/pair/"+ referencias.getConverterDe()
+                    +"/"+ referencias.getConverterPara()+"/"+ referencias.getQuantia());
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(endereco)
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            Conversao conversao = gson.fromJson(json, Conversao.class);
+            System.out.println(conversao);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
